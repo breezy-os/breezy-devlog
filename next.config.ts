87a9e1c0aab081e8b033298672ba16bd02706abc
Ben <ben@zenittini.dev>
2026-04-14 12:52:03 -0400

    Clean up for final build.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['localhost', '10.0.0.179'],
+  output: 'export',
 };
 
 export default nextConfig;
